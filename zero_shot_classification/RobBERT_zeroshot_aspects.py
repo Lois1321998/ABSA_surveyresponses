5be7ce12ecf271f53791e7feedb1f46db0210567
Lois1321998 <72651225+Lois1321998@users.noreply.github.com>
2023-06-29 11:27:02 +0200

    Add files via upload

---
+++ zero_shot_classification/RobBERT_zeroshot_aspects.py
@@ -0,0 +1,153 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import f1_score, classification_report
+import transformers
+import torch
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import BertTokenizer, BertModel, BertConfig, RobertaTokenizer, Robertamodel, RobertaConfig
+from transformers import AutoTokenizer, AutoModel, pipeline, AutoModelForSequenceClassification
+
+import ast
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+
+import time
+
+df_train = pd.read_csv("../data/zeroshot_classification/preprocessed_train_ABSA_withoutDA.csv", sep=';', index_col=0)
+df_test = pd.read_csv("../data/zeroshot_classification/preprocessed_test_ABSA.csv", sep=';', index_col=0)
+
+df_train['common_annotation'] = df_train['common_annotation'].apply(lambda x: ast.literal_eval(x))
+df_train['clean_annotation'] = df_train['clean_annotation'].apply(lambda x: ast.literal_eval(x))
+df_test['common_annotation'] = df_test['common_annotation'].apply(lambda x: ast.literal_eval(x))
+df_test['clean_annotation'] = df_test['clean_annotation'].apply(lambda x: ast.literal_eval(x))
+
+class ZeroShotMultilabelClassifier:
+    def __init__(self, model_name):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    def predict(self, sentence, aspect_labels, threshold=0.5):
+        inputs = self.tokenizer([f"{sentence} [SEP] {label}" for label in aspect_labels], 
+                                 padding=True, truncation=True, return_tensors="pt")
+        with torch.no_grad():
+            logits = self.model(**inputs).logits
+
+        probabilities = torch.softmax(logits, dim=1)[:, 1].tolist()
+
+        # Filter aspectcs based on threshold
+        predicted_aspects = [aspect_labels[i] for i, prob in enumerate(probabilities) if prob >= threshold]
+
+        return probabilities, predicted_aspects
+
+def predict_aspects_with_threshold(row, classifier, aspect_labels, threshold):
+    probabilities, predicted_aspects = classifier.predict(row["text"], aspect_labels, threshold)
+    #print(probabilities)
+    return predicted_aspects
+
+def calculate_f1_score(y_true, y_pred, mlb):
+    y_true_binarized = mlb.transform(np.array(y_true))
+    y_pred_binarized = mlb.transform(np.array(y_pred))
+    print(classification_report(y_true_binarized, y_pred_binarized, target_names=mlb.classes_))
+    print()
+    return f1_score(y_true_binarized, y_pred_binarized, average='macro')
+
+def grid_search_threshold(df, model_name, aspect_labels, thresholds):
+    # Initialize the classifier with the specified model
+    classifier = ZeroShotMultilabelClassifier(model_name)
+    
+    # Fit the MultiLabelBinarizer on the aspect labels
+    mlb = MultiLabelBinarizer()
+    mlb.fit([aspect_labels])
+
+
+
+    best_f1 = 0
+    best_threshold = None
+
+    for threshold in thresholds:
+        # Get the predicted aspects for each row
+        df["predicted_aspects"] = df.apply(predict_aspects_with_threshold, axis=1, classifier=classifier, aspect_labels=aspect_labels, threshold=threshold)
+        # Calculate the F1-score
+
+        f1 = calculate_f1_score(df["clean_annotation"].tolist(), df["predicted_aspects"].tolist(), mlb)
+        print(threshold, f1)
+        # Update the best threshold and F1-score if necessary
+        if f1 > best_f1:
+            best_f1 = f1
+            best_threshold = threshold
+            break
+
+    return best_threshold, best_f1
+
+# Define the BERTje model name and aspect labels
+model_name = 'pdelobelle/robbert-v2-dutch-base'
+aspect_labels = ['contact','persoonlijke aandacht', 'salaris', 'communicatie','roosters en planning', 'afspraken']
+
+# Define the thresholds to search over
+thresholds = np.linspace(0.45, 0.65, 9)
+
+best_threshold, best_f1 = grid_search_threshold(df_train, model_name, aspect_labels, thresholds)
+
+print(f"Best threshold: {best_threshold}")
+print(f"Best F1-score: {best_f1}")
+
+# Initialize the classifier with the best threshold and specified model name
+classifier = ZeroShotMultilabelClassifier(model_name)
+
+# Get the predicted aspects for each row using the best threshold
+df_train["predicted_aspects"] = df_train.apply(predict_aspects_with_threshold, axis=1, classifier=classifier, aspect_labels=aspect_labels, threshold=best_threshold)
+df_test["predicted_aspects"] = df_test.apply(predict_aspects_with_threshold, axis=1, classifier=classifier, aspect_labels=aspect_labels, threshold=best_threshold)
+
+# Print the resulting dataframe with predicted aspects
+print(df_test[["text", "predicted_aspects"]])
+
+df_test.to_csv("../data/predictions/RobBERT_zeroshot_aspects_withoutDA.csv", sep=';')
+
+mlb = MultiLabelBinarizer()
+y_true = mlb.fit_transform(df_train['clean_annotation'])
+y_pred = mlb.transform(df_train['predicted_aspects'])
+
+# Print the classification report
+print(classification_report(y_true, y_pred, target_names=mlb.classes_))
+
+
+# Print the classification report
+report = classification_report(y_true, y_pred, target_names=mlb.classes_, output_dict=True)
+print(report)
+
+rounded_dict = {}
+for key, inner_dict in report.items():
+    rounded_inner_dict = {}
+    for inner_key, value in inner_dict.items():
+        rounded_inner_dict[inner_key] = round(value, 2)
+    rounded_dict[key] = rounded_inner_dict
+
+print(rounded_dict)
+
+df_output = pd.DataFrame(rounded_dict).transpose()
+df_output.to_csv('../classification_reports/classification_report_train_RobBERT_zeroshot_aspects.csv', sep=';')
+
+mlb = MultiLabelBinarizer()
+y_true = mlb.fit_transform(df_test['clean_annotation'])
+y_pred = mlb.transform(df_test['predicted_aspects'])
+
+# Print the classification report
+print(classification_report(y_true, y_pred, target_names=mlb.classes_))
+
+# Print the classification report
+report = classification_report(y_true, y_pred, target_names=mlb.classes_, output_dict=True)
+#print(report)
+
+rounded_dict = {}
+for key, inner_dict in report.items():
+    rounded_inner_dict = {}
+    for inner_key, value in inner_dict.items():
+        rounded_inner_dict[inner_key] = round(value, 2)
+    rounded_dict[key] = rounded_inner_dict
+
+print(rounded_dict)
+
+df_output = pd.DataFrame(rounded_dict).transpose()
+df_output.to_csv('../classification_reports/classification_report_test_RobBERT_zeroshot_aspects.csv', sep=';')
+
+print("--- %s seconds ---" % (time.time() - start_time))
---
+++ zero_shot_classification/RobBERT_zeroshot_sentiments.py
@@ -0,0 +1,149 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import f1_score, classification_report
+import transformers
+import torch
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import BertTokenizer, BertModel, BertConfig
+from transformers import AutoTokenizer, AutoModel, pipeline, AutoModelForSequenceClassification
+
+import ast
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+
+import time
+
+start_time = time.time()
+
+class BinarySentimentClassifier:
+    def __init__(self, model_name):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    def predict(self, sentence, aspect, sentiment_labels):
+        inputs = self.tokenizer([f"{sentence} [SEP] {aspect} [SEP] {label}" for label in sentiment_labels], 
+                                 padding=True, truncation=True, return_tensors="pt")
+        with torch.no_grad():
+            logits = self.model(**inputs).logits
+
+        probabilities = torch.softmax(logits, dim=1)[:, 1].tolist()
+
+        # Choose the label with the highest probability
+        predicted_sentiment = sentiment_labels[probabilities.index(max(probabilities))]
+
+        return predicted_sentiment
+
+def calculate_f1_score(y_true, y_pred):
+    print(classification_report(y_true, y_pred))
+    print()
+    return f1_score(y_true, y_pred, average='binary', pos_label='positief')
+
+def transform_sentiment_data(df, annotations='common_annotation'):
+    # Create a new dataframe to store the transformed data
+    new_data = []
+
+    # Iterate over each row in the original dataframe
+    for index, row in df.iterrows():
+        text = row['text']
+        labels = row[annotations]
+        clean_annotation = row['clean_annotation']
+        
+        # Iterate over each label in the row
+        for label in ast.literal_eval(labels):
+            #print(label)
+            new_row = {'text': text, 'label': label, 'sentiment': [label[-3:] if label != 'niks' else ''], 'common_annotation': labels, 'clean_annotation': clean_annotation}
+            new_data.append(new_row)
+
+    # Create the transformed dataframe
+    transformed_df = pd.DataFrame(new_data)
+    return transformed_df
+
+df_train = pd.read_csv("../data/zeroshot_classification/preprocessed_train_ABSA_withoutDA.csv", sep=';')
+df_test = pd.read_csv("../data/zeroshot_classification/preprocessed_test_ABSA.csv", sep=';')
+
+df_train = transform_sentiment_data(df_train)
+df_test = transform_sentiment_data(df_test)
+
+df_train['label'] = df_train['label'].str.rstrip('_NEG').str.rstrip('_POS').replace('persoonlijke-aandacht', 'persoonlijke aandacht').replace('roosters-planning', 'roosters en planning')
+df_train['sentiment'] = df_train['sentiment'].apply(lambda x: x[0]).replace('NEG', 'negatief').replace('POS', 'positief')
+df_train = df_train[df_train['sentiment'].apply(lambda x: x != '')]
+
+df_test['label'] = df_test['label'].str.rstrip('_NEG').str.rstrip('_POS').replace('persoonlijke-aandacht', 'persoonlijke aandacht').replace('roosters-planning', 'roosters en planning')
+df_test['sentiment'] = df_test['sentiment'].apply(lambda x: x[0]).replace('NEG', 'negatief').replace('POS', 'positief')
+df_test = df_test[df_test['sentiment'].apply(lambda x: x != '')]
+
+# Initialize the classifier with the specified model
+model_name = 'wietsedv/bert-base-dutch-cased'
+sentiment_labels = ['positief', 'negatief']
+classifier = BinarySentimentClassifier(model_name)
+
+# Get the predicted sentiment for each row
+df_train["predicted_sentiment"] = df_train.apply(lambda row: classifier.predict(row["text"], row["common_annotation"], sentiment_labels), axis=1)
+
+# Calculate the F1-score
+f1 = calculate_f1_score(df_train["sentiment"].tolist(), df_train["predicted_sentiment"].tolist())
+print("F1-score:", f1)
+
+y_true = df_train['sentiment']
+y_pred = df_train['predicted_sentiment']
+
+# Print the classification report
+print(classification_report(y_true, y_pred))
+
+
+# Print the classification report
+report = classification_report(y_true, y_pred, output_dict=True)
+print(report)
+
+rounded_dict = {}
+for key, value in report.items():
+    if isinstance(value, dict):
+        rounded_inner_dict = {inner_key: round(inner_value, 2) for inner_key, inner_value in value.items()}
+        rounded_dict[key] = rounded_inner_dict
+    else:
+        rounded_dict[key] = round(value, 2)
+
+print(rounded_dict)
+
+df_output = pd.DataFrame(rounded_dict).transpose()
+df_output.to_csv('../classification_reports/classification_report_train_BERTje_zeroshot_sentiments_withoutDA.csv', sep=';')
+
+# Initialize the classifier with the specified model
+tokenizer = RobertaTokenizerFast.from_pretrained('pdelobelle/robbert-v2-dutch-base')
+sentiment_labels = ['positief', 'negatief']
+classifier = BinarySentimentClassifier(model_name)
+
+# Get the predicted sentiment for each row
+df_test["predicted_sentiment"] = df_test.apply(lambda row: classifier.predict(row["text"], row["common_annotation"], sentiment_labels), axis=1)
+
+# Calculate the F1-score
+f1 = calculate_f1_score(df_test["sentiment"].tolist(), df_test["predicted_sentiment"].tolist())
+print("F1-score:", f1)
+
+df_test.to_csv("../data/predictions/RobBERT_zeroshot_sentiments_withoutDA.csv", sep=';')
+
+y_true = df_test['sentiment']
+y_pred = df_test['predicted_sentiment']
+
+# Print the classification report
+print(classification_report(y_true, y_pred, target_names=['positief', 'negatief']))
+
+
+# Print the classification report
+report = classification_report(y_true, y_pred, target_names=['positief', 'negatief'], output_dict=True)
+print(report)
+
+rounded_dict = {}
+for key, value in report.items():
+    if isinstance(value, dict):
+        rounded_inner_dict = {inner_key: round(inner_value, 2) for inner_key, inner_value in value.items()}
+        rounded_dict[key] = rounded_inner_dict
+    else:
+        rounded_dict[key] = round(value, 2)
+
+print(rounded_dict)
+
+df_output = pd.DataFrame(rounded_dict).transpose()
+df_output.to_csv('../classification_reports/classification_report_test_RobBERT_zeroshot_sentiments_withoutDA.csv', sep=';')
+
+print("--- %s seconds ---" % (time.time() - start_time))
